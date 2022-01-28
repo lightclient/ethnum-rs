e889eda9ae9553aad87b1b0584267ae629cb5216
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2022-01-28 10:38:43 +0000

    started implementing intrinsics

--- src/intrinsics/native/shr.rs
+++ src/intrinsics/native/shr.rs
@@ -4,13 +4,39 @@ use crate::{int::I256, uint::U256};
 use core::mem::MaybeUninit;
 
 #[inline]
-pub fn sar2(_: &mut I256, _: u32) {
-    todo!()
+pub fn sar2(r: &mut I256, a: u32) {
+    debug_assert!(a < 256, "shr intrinsic called with overflowing shift");
+
+    let (hi, lo) = if a == 0 {
+        return;
+    } else if a < 128 {
+        (
+            r.high() >> a,
+            (*r.low() as u128) >> a | ((*r.high() as u128) << (128 - a)),
+        )
+    } else {
+        (r.high() >> 128, ((*r.high() as u128) >> (a & 0x7f)))
+    };
+
+    *r = I256::from_words(hi, lo as _);
 }
 
 #[inline]
-pub fn sar3(_: &mut MaybeUninit<I256>, _: &I256, _: u32) {
-    todo!()
+pub fn sar3(r: &mut MaybeUninit<I256>, a: &I256, b: u32) {
+    debug_assert!(b < 256, "shr intrinsic called with overflowing shift");
+
+    let (hi, lo) = if b == 0 {
+        (*a.high(), (*a.low() as u128))
+    } else if b < 128 {
+        (
+            a.high() >> b,
+            (*a.low() as u128) >> b | ((*a.high() as u128) << (128 - b)),
+        )
+    } else {
+        (a.high() >> 128, (*a.high() as u128) >> (b & 0x7f))
+    };
+
+    r.write(I256::from_words(hi, lo as _));
 }
 
 #[inline]
--- src/intrinsics/native/sub.rs
+++ src/intrinsics/native/sub.rs
@@ -29,6 +29,8 @@ pub fn usubc(r: &mut MaybeUninit<U256>, a: &U256, b: &U256) -> bool {
 }
 
 #[inline]
-pub fn isubc(_: &mut MaybeUninit<I256>, _: &I256, _: &I256) -> bool {
-    todo!()
+pub fn isubc(r: &mut MaybeUninit<I256>, a: &I256, b: &I256) -> bool {
+    sub3(cast!(uninit: r), cast!(ref: a), cast!(ref: b));
+    let s = unsafe { r.assume_init_ref() };
+    (*b.high() >= 0 && s > a) || (*b.high() < 0 && s <= a)
 }
